a15fb962454e3c13c98fdabffc1f80308936917b
andrewMorrison20 <158091930+andrewMorrison20@users.noreply.github.com>
2024-05-23 18:37:02 +0100

    Update DeviceSearch.java
    update java doc

--- SmartDevice/src/smartDevice/DeviceSearch.java
+++ SmartDevice/src/smartDevice/DeviceSearch.java
@@ -4,7 +4,15 @@ import java.util.ArrayList;
 
 public class DeviceSearch {
 
-	
+	//polymorphic as any device can have the required attributes
+	/**
+	 * takes an arraylist and searches the list for any object that matches the room
+	 * passed and returns the objects in an Array list
+	 * 
+	 * @param allDevices
+	 * @param room
+	 * @return results
+	 */
 	public static ArrayList<Device> searchByRoom(ArrayList<Device> allDevices, Room room) throws IllegalArgumentException{
 
 		ArrayList<Device> results = new ArrayList<Device>();
@@ -17,7 +25,16 @@ public class DeviceSearch {
 		} else {throw new IllegalArgumentException("List cannot be null");}
 		return results;
 	}
-
+	//only radiator has temp so not polymorphic
+	/**
+	 * Takes an arraylist of type radiator and searches for those radiators within
+	 * the defined range returns the list as an arraylist
+	 * 
+	 * @param allRadiators
+	 * @param tempLowerLimit
+	 * @param tempUpperLimit
+	 * @return
+	 */
 	public ArrayList<SmartRadiator> searchBytemp(ArrayList<SmartRadiator> allRadiators, double tempLower, double tempUpper) {
 		ArrayList<SmartRadiator> results = new ArrayList<SmartRadiator>();
 		if(tempLower>tempUpper || allRadiators.size()==0) {throw new IllegalArgumentException("Invalid Inputs");}
